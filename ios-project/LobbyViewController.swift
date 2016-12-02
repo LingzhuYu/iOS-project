17c2c5ad4503d999a0e4136355174cea8ad16054
Jordan Hamade <a00932659@btechgames-imac-20.local>
2016-12-02 12:51:03 -0800

    mapView in host settings now grabs coordinates ready to send

--- ios-project/LobbyViewController.swift
+++ ios-project/LobbyViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Firebase
-
+import MapKit
 
 
 class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -19,6 +19,9 @@ class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     @IBOutlet weak var HostSettingsButton: UIButton!
     
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    
     
     fileprivate var db: FIRDatabaseReference!
     fileprivate var _refHandle: FIRDatabaseHandle!   // not observing anything
@@ -28,7 +31,10 @@ class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDat
     public var hostId : String = ""
     var players = [LobbyUser]()
     var currentUser : LobbyUser?
-
+    
+    var mapCoordinate1 : CLLocationCoordinate2D?
+    var mapCoordinate2 : CLLocationCoordinate2D?
+    
     // let lobby : Lobby = Lobby()
     
     override func viewDidLoad() {
@@ -38,9 +44,14 @@ class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDat
         self.tableView.delegate = self 
         
         configureDatabase()
+        
+        startGameButton.addTarget(self, action: #selector(startGame), for: .touchUpInside)
+        
 
-        HostSettingsButton.addTarget(self, action: #selector(startMap), for: .touchUpInside)
-        HostSettingsButton.backgroundColor = UIColor.clear
+    }
+    
+    func startGame(){
+        
     }
     
     fileprivate func configureDatabase() {
@@ -67,6 +78,7 @@ class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // retrieve values 
         // var lobby = self.db.child("lobbies").child(gameId)
         
+
     }
     
     
@@ -74,6 +86,13 @@ class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         //grabs the host's device ID from the database
         hostId = devices.childSnapshot(forPath: "hostId").value as! String
+        
+        if(hostId == deviceId){
+            HostSettingsButton.addTarget(self, action: #selector(startMap), for: .touchUpInside)
+            HostSettingsButton.backgroundColor = UIColor.clear
+        }else{
+            HostSettingsButton.isHidden = true
+        }
         
     }
 
@@ -150,7 +169,7 @@ class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
 
     deinit {
-        self.db.child("lobbies").child(gameId).removeObserver(withHandle: _refHandle)
+       // self.db.child("lobbies").child(gameId).removeObserver(withHandle: _refHandle)
     }
     
     
@@ -173,12 +192,19 @@ class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
     //Start of host map settings stuff
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let guest = segue.destination as! LobbyMapSelectView
+        if (segue.identifier == "mapSegue") {
+            let guest = segue.destination as! LobbyMapSelectView
+            guest.lobby = self
+        } else if (segue.identifier == "gameSegue") {
+            // go to game
+        }
     }
     
     func startMap(){
         performSegue(withIdentifier: "mapSegue" , sender: nil)
     }
+    
+
     
     //End of host map settings stuff
 }
